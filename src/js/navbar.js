f3a2246a15209039ee3f369d090a8a52fbf9aea6
Cornell Daly <cmdaly@stetson.edu>
2017-09-24 00:53:22 -0400

    Changed normalize to normalize-styl. Began removing jQuery from JS scripts.

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,31 +1,32 @@
 'use strict';
 
-import $ from 'jquery';
-
 export default class BasisNavbar {
   constructor() {
-    this.items = $('[data-c="navbar__item"][aria-haspopup="true"], [data-c="navbar__subitem"][aria-haspopup="true"]');
+    this.items = document.querySelectorAll('[data-c="navbar__item"][aria-haspopup="true"], [data-c="navbar__subitem"][aria-haspopup="true"]');
     this.setListener();
   }
 
   setListener() {
-    this.items.each((i, e) => {
-      const item = $(e);
-      item.on('mouseover focus', (event) => {
-        this.show(item.children('[data-c="navbar__submenu"]'));
-      });
+    this.items.forEach((item, index, array) => {
+      item.addEventListener('mouseover', (event) => {
+        this.show(item.querySelector('[data-c="navbar__submenu"]'))
+      })
 
-      item.on('mouseleave', (event) => {
-        this.hidden(item.children('[data-c="navbar__submenu"]'));
-      });
-    });
+      item.addEventListener('focus', (event) => {
+        this.show(item.querySelector('[data-c="navbar__submenu"]'))
+      })
+
+      item.addEventListener('mouseleave', (event) => {
+        this.hidden(item.querySelector('[data-c="navbar__submenu"]'))
+      })
+    })
   }
 
   show(submenu) {
-    submenu.attr('aria-hidden', 'false');
+    submenu.setAttribute('aria-hidden', 'false');
   }
 
   hidden(submenu) {
-    submenu.attr('aria-hidden', 'true');
+    submenu.setAttribute('aria-hidden', 'true');
   }
 }
